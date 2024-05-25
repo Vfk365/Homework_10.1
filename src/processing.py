2b359816e4d8675f82e3c2e46a2c61de634f008f
Anton <anton.wisp@gmail.com>
2024-05-25 15:52:51 +0300

    creating processing.py

---
+++ src/processing.py
@@ -0,0 +1,13 @@
+def get_dictionary_key(list_dict: list, state: str = "EXECUTED") -> list:
+    """Функция которая фильтрует список словарей по ключу state"""
+    filtred_list = []
+    for ld in list_dict:
+        if ld.get("state") == state:
+            filtred_list.append(ld)
+    return filtred_list
+
+
+def get_date_sorted(list_dict: list, direction: bool = True) -> list:
+    """Функция сортирует словари по дате"""
+    sorted_list = sorted(list_dict, key=lambda x: x["date"], reverse=direction)
+    return sorted_list

f7e10a2cec3481773652354661821b29e08b6162
Anton <anton.wisp@gmail.com>
2024-07-04 14:24:37 +0300

    create logs

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,23 @@
+from logger import set_logger_masks
+
+logger = set_logger_masks()
+
+
 def get_mask_card(number: str) -> str:
     """Функция принимает строку и возвращает маску карты"""
+    logger.info("Запущена функция get_mask_card")
     new_string = f"{number[0:4]} {number[4:6]}** **** {number[12:]}"
+    logger.info("Функция вернула замаскированный номер карты")
     return new_string
 
 
 def get_mask_account(number: str) -> str:
     """Функция принимает строку и возвращает маску счёта"""
+    logger.info("Запущена функция get_mask_account")
     new_string = f"**{number[-4:]}"
+    logger.info("Функция вернула замаскированный номер счёта")
     return new_string
+
+
+result = get_mask_card("123456789101")
+result = get_mask_account("12345621859856789101")
--- src/utils.py
+++ src/utils.py
@@ -1,19 +1,26 @@
 import json
-import os
 from typing import Dict, List
+
+from logger import set_logger
+
+logger = set_logger()
 
 
 def data_transactions(file_path: str) -> List[Dict]:
-    '''Функция, которая принимает на вход путь до JSON-файла и
-    возвращает список словарей с данными о финансовых транзакциях.'''
+    """Функция, которая принимает на вход путь до JSON-файла и
+    возвращает список словарей с данными о финансовых транзакциях."""
+    logger.info("Запущена функция data_transactions")
     try:
         with open(file_path, "r", encoding="utf-8") as file:
             list_data_transactions = json.load(file)
             if isinstance(list_data_transactions, list):
+                logger.info("Функция вернула список данных")
                 return list_data_transactions
             else:
+                logger.warning("Функция вернула пустой список данных")
                 return []
-    except (FileNotFoundError, json.JSONDecodeError):
+    except (FileNotFoundError, json.JSONDecodeError) as ex:
+        logger.error(f"Ошибка {ex}")
         return []
 
 
